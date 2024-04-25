5018cac28d4f8ded822b9c8b211614d9bd130a8e
TouristOnAMission <maikl.mikhael@yandex.ru>
2024-04-25 23:36:31 +0300

    Updates

--- src/constants/constants.ts
+++
@@ -1,5 +0,0 @@
-export const NUMBER_OF_PAGES_DISPLAYED = 5;
-export const POSTER_API_PATH = 'https://image.tmdb.org/t/p/w500';
-export const API_URL = 'https://api.themoviedb.org/3';
-export const SCROLL_SPEED = 1;
-export const LENGTH_TO_START_SCROLLING = 5;
